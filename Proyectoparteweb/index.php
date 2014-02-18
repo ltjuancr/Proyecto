1c8a6c5839ff294e295ecb6c036a4fab974021f0
ltjuancr <juan_gonzalez93@hotmail.com>
2014-02-18 16:44:45 -0600

    Formulario
    tengo problemas con l variable $_post

--- Proyectoparteweb/index.php
+++ Proyectoparteweb/index.php
@@ -6,7 +6,9 @@
 
 <BODY>
 	<center>
-<form>
+		<p>Todos los campos son obligatorios</p>
+
+<form  method="POST" name="formulario">
 Nombre: <input type="text" name="nombre">
 <br><br>
 Apellido: <input type="text" name="apellido">
@@ -17,7 +19,23 @@ Telefono: <input type="text" name="telefono">
 <br><br>
 Cédula: <input type="text" name="cedula">
 <br><br>
-<input type="Submit" value="Registrar">
+<input name="reset" value="Borrar datos" type="reset">
+
+<input name="submit" value="Enviar datos" type="submit"  onclick='<?php hola(); ?>'>
+
+<?php
+function hola()
+{
+echo $_POST['nombre'];
+echo $_POST['apellido'];
+echo $_POST['correo'];
+echo $_POST['telefono'];
+echo $_POST['cedula'];
+};
+?>
+
+<b> <?php hola(); ?> </b>
+
 </form>
 </center>
 </BODY>
