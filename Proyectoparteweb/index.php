a88e3f2796a997a47016453a123ece635b37972c
ltjuancr <juan_gonzalez93@hotmail.com>
2014-02-18 15:37:26 -0600

    Estructura web

--- Proyectoparteweb/index.php
+++ Proyectoparteweb/index.php
@@ -1,8 +1,24 @@
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<HTM>
+<HEAD>
+<TITLE> Formulario</TITLE>
+</HEAD>
+<center>Bienvenido</center>
 
-</body>
-</html>
+<BODY>
+	<center>
+<form>
+Nombre: <input type="text" name="nombre">
+<br><br>
+Apellido: <input type="text" name="apellido">
+<br><br>
+Correo: <input type="text" name="correo">
+<br><br>
+Telefono: <input type="text" name="telefono">
+<br><br>
+Cédula: <input type="text" name="cedula">
+<br><br>
+<input type="Submit" value="Registrar">
+</form>
+</center>
+</BODY>
+</HTM>
